f64478e8b0f14f7365f4f19819b2b7f83381def3
lemonsot <124886237+lemonsot@users.noreply.github.com>
2023-05-05 14:31:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -458,7 +458,7 @@ async def save_shortlink(client, message):
     try:
         _, url, api = message.text.split(" ", 2)
     except:
-        return await message.reply_text("<b>Command Incomplete:-\n\ngive me a shortlink & api along with the command...\n\nEx:- <code>/shortlink mdisklink.link 5843c3cc645f5077b2200a2c77e0344879880b3e</code>")
+        return await message.reply_text("<b>Command Incomplete:-\n\ngive me a shortlink & api along with the command...\n\nEx:- <code>/shortlink omegalinks.in 0d579f14e443eea095bb71c3b57f23d715dff555</code>")
     
     try:
         shortzy = Shortzy(api_key=api, base_site=url)
